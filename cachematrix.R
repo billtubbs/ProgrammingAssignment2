a88bd910bfd1b8b9d23f7f0bb8dfd33909044836
Bill Tubbs <bill.tubbs@me.com>
2014-06-22 15:40:27 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix, cacheSolve
+## These two functions can be used to create a special object that stores 
+## a square matrix and caches its inverse so that the inverse does not have 
+## to be recalculated if the matrix has not changed.
 
-## Write a short comment describing this function
+## 1. makeCacheMatrix 
+## creates a list containing functions to:
+## - set the value of the matrix, 
+## - get the value of the matrix
+## - set the inverse matrix
+## - get the inverse matrix
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(m = matrix()) {
+    mI <- NULL
+    set <- function(y) {
+        m <<- y
+        mI <<- NULL
+    }
+    get <- function() m
+    setmI <- function(matrixInv) mI <<- matrixInv
+    getmI <- function() mI
+    list(set = set, get = get,
+         setmI = setmI,
+         getmI = getmI)
 }
 
 
-## Write a short comment describing this function
+## 2. cacheSolve
+## returns the matrix inverse either from the cache or caclulates
+## it if the cache is empty.
+## NOTE: the matrix passed to cacheSolve must be square
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(m, ...) {
+        ## Return a matrix that is the inverse of 'm'
+    mI <- m$getmI()
+    if(!is.null(mI)) {
+        message("getting cached data")
+        return(mI)
+    }
+    data <- m$get()
+    mI <- solve(data, ...)
+    m$setmI(mI)
+    mI
 }
